1bb1ac8e1bdf7b70d5ace10bd1f86372fa14d052
Axel <axosar@gmail.com>
2021-09-30 17:51:39 -0500

    next document page

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,5 @@
 import Page from '../components/Page';
 
 export default function IndexPage() {
-  return (
-    <Page cool="Hey cool">
-      <h1>Hello</h1>
-    </Page>
-  );
+  return <h1>Hello</h1>;
 }
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,9 +1,3 @@
-import Page from '../components/Page';
-
 export default function SellPage() {
-  return (
-    <Page cool="Hey cool">
-      <h1>Hello</h1>
-    </Page>
-  );
+  return <h1>Hello</h1>;
 }
